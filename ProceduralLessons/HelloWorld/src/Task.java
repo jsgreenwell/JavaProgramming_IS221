8972d858d9f317b2cfafc08ba64f6a3dbbf88d6c
JGreenwell <jsgreenwell83@gmail.com>
2024-01-10 17:39:17 -0600

    removed the answer to the Hello World Lesson

--- ProceduralLessons/HelloWorld/src/Task.java
+++ ProceduralLessons/HelloWorld/src/Task.java
@@ -2,6 +2,5 @@ public class Task {
     static void hello() {
         // Remember to use print not println
         // remove this line and enter your code
-        System.out.println("Hello, World!");
     }
 }
